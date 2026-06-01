7f5da004f17ba177bccd2348e3d6c1ea04910493
Oskle <93563559+Oskle@users.noreply.github.com>
2026-05-31 22:21:54 -0500

    Delete ProyectFlutter directory

--- ProyectFlutter/lib/domain/repositories/dashboard_repository.dart
+++
@@ -1,6 +0,0 @@
-import '../entities/dashboard_stats.dart';
-
-abstract class DashboardRepository {
-  Future<List<EvaluationResult>> getResultsForStudent(String studentUid);
-  Future<DashboardConsolidated> getResultsForTeacher(String cycleId);
-}
