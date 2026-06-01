fe8f5b330f9ef161d34564f9c319985c81a89a8c
Oskle <93563559+Oskle@users.noreply.github.com>
2026-05-31 22:12:09 -0500

    Delete ProyectoReactNative directory

--- ProyectoReactNative/src/domain/repositories/dashboardRepository.ts
+++
@@ -1,6 +0,0 @@
-import { DashboardConsolidated, EvaluationResult } from '@/domain/entities/academic';
-
-export interface DashboardRepository {
-  getResultsForStudent(input: { studentUid: string; studentEmail: string }): Promise<EvaluationResult[]>;
-  getResultsForTeacher(cycleId: string): Promise<DashboardConsolidated>;
-}
